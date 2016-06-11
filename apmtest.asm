2df970a93fd83d4420a25259c165434d546bb9fb
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2016-06-11 20:58:11 +0200

    Add GDT helper macros/equs

--- apmtest.asm
+++ apmtest.asm
@@ -59,15 +59,35 @@ test_pmode32:
     mov eax, 0x2F00 | '*'
     mov ecx, 80
     rep stosw
-.spin:
-    jmp .spin
+
+.hang:
+    jmp .hang
+
+leave_pmode:
+    ; 1. Disable interrupts
+    cli
+    ; 2. Turn of paging
+    ; 3. 
 
     bits 16
 
+GDTF_RW         equ 0x0002 ; For code segments RW=1 means the segment is readable, for data segments RW=1 means the segment is writable
+GDTF_EXECUTABLE equ 0x0008
+GDTF_SYSTEM     equ 0x0010 ; When the S (descriptor type) flag in a segment descriptor is clear, the descriptor type is a system descriptor (IA32 Vol. 3A 3.5):w
+GDTF_PRESENT    equ 0x0080
+GDTF_32BIT      equ 0x4000 ; Sz=0 16-bit, Sz=1 32-bit
+GDTF_PAGE_GRAN  equ 0x8000 ; Granularity Gr=0 means byte granularity, Gr=1 means page (4K) granularity
+
+%define GDT_ENTRY(base, limit, flags) ((limit&0xFFFF) | ((base & 0xFFFFFF) << 16) | ((flags & 0xF0FF) << 40) | ((limit>>16) << 48) | ((base>>24) << 56))
+
+GDT_ENTRY_NULL   equ GDT_ENTRY(0, 0x0, 0x0)
+GDT_ENTRY_CODE32 equ GDT_ENTRY(0, 0xFFFFF, GDTF_EXECUTABLE | GDTF_RW | GDTF_SYSTEM | GDTF_PRESENT | GDTF_32BIT | GDTF_PAGE_GRAN) ; code X executable R (readable) P (present) 4k Gran. 32-bit Ring0
+GDT_ENTRY_DATA32 equ GDT_ENTRY(0, 0xFFFFF, GDTF_RW | GDTF_PRESENT | GDTF_SYSTEM | GDTF_32BIT | GDTF_PAGE_GRAN) ; data W writable  P (present) 4k Gran. 32-bit Ring0
+
 gdt:
-    dq      0x0000000000000000 ; null
-    dq      0x00CF9A000000FFFF ; code X executable R (readable) P (present) 4k Gran. 32-bit Ring0
-    dq      0x00CF92000000FFFF ; data W writable  P (present) 4k Gran. 32-bit Ring0
+    dq GDT_ENTRY_NULL   ; 0x0000000000000000
+    dq GDT_ENTRY_CODE32 ; 0x00CF9A000000FFFF
+    dq GDT_ENTRY_DATA32 ; 0x00CF92000000FFFF
 gdt_end:
 
 gdtr:
