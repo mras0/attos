ea636a2a0b5af87587ed5c1b7ce977856f96fc28
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2016-06-26 12:33:35 +0200

    Smarter PCI probe + handle multi function devices.

--- stage3/attos/pci.cpp
+++ stage3/attos/pci.cpp
@@ -39,6 +39,9 @@ uint32_t read_config_dword(device_address dev, uint8_t reg)
     return __indword(config_data_port);
 }
 
+constexpr uint8_t header_type_device_mask         = 0x03; // 0x00 = general device, 0x01 = PCI-to-PCI bridge, 0x02 = CardBus bridge
+constexpr uint8_t header_type_multi_function_mask = 0x80;
+
 #pragma pack(push, 1)
 struct config_area {
     uint16_t vendor;
@@ -94,6 +97,24 @@ constexpr uint16_t vendor_ensoniq = 0x1274; // Ensoniq
 constexpr uint16_t vendor_vmware  = 0x15AD; // VMware Inc
 constexpr uint16_t vendor_intel   = 0x8086; // Intel Corporation
 
+constexpr uint16_t device_class_scsi_controller         = 0x0100;
+constexpr uint16_t device_class_ide_controller          = 0x0101;
+
+constexpr uint16_t device_class_ethernet_controller     = 0x0200;
+
+constexpr uint16_t device_class_vga_controller          = 0x0300;
+
+constexpr uint16_t device_class_audio_device            = 0x0401;
+
+constexpr uint16_t device_class_host_bridge             = 0x0600;
+constexpr uint16_t device_class_isa_bridge              = 0x0601;
+constexpr uint16_t device_class_pci_bridge              = 0x0604;
+constexpr uint16_t device_class_other_bridge            = 0x0680;
+
+constexpr uint16_t device_class_other_system_peripheral = 0x0880;
+
+constexpr uint16_t device_class_usb_controller          = 0x0C03;
+
 struct vendor_info {
     uint16_t vendor;
     const char* text;
@@ -112,17 +133,23 @@ struct device_info {
     const char* text;
 };
 const device_info known_devices[] = {
-    { vendor_lsi     , 0x0030 , 0x0100 , "53c1030 PCI-X Fusion-MPT Dual Ultra320 SCSI"  },
-    { vendor_ensoniq , 0x1371 , 0x0401 , "ES1371 / Creative Labs CT2518/ES1373"         },
-    { vendor_vmware  , 0x0405 , 0x0300 , "SVGA II Adapter"                              },
-    { vendor_vmware  , 0x0790 , 0x0604 , "PCI bridge"                                   },
-    { vendor_vmware  , 0x07A0 , 0x0604 , "PCI Express Root Port"                        },
-    { vendor_intel   , 0x100F , 0x0200 , "82545EM Gigabit Ethernet Controller (Copper)" },
-    { vendor_intel   , 0x1237 , 0x0600 , "440FX - 82441FX PMC [Natoma]"                 },
-    { vendor_intel   , 0x7000 , 0x0601 , "82371SB PIIX3 ISA [Natoma/Triton II]"         },
-    { vendor_intel   , 0x7110 , 0x0601 , "82371AB/EB/MB PIIX4 ISA"                      },
-    { vendor_intel   , 0x7190 , 0x0600 , "440BX/ZX/DX - 82443BX/ZX/DX Host bridge"      },
-    { vendor_intel   , 0x7191 , 0x0604 , "440BX/ZX/DX - 82443BX/ZX/DX AGP bridge"       },
+    { vendor_lsi     , 0x0030 , device_class_scsi_controller         , "53c1030 PCI-X Fusion-MPT Dual Ultra320 SCSI"  },
+    { vendor_ensoniq , 0x1371 , device_class_audio_device            , "ES1371 / Creative Labs CT2518/ES1373"         },
+    { vendor_vmware  , 0x0405 , device_class_vga_controller          , "SVGA II Adapter"                              },
+    { vendor_vmware  , 0x0740 , device_class_other_system_peripheral , "Virtual Machine Communication Interface"      },
+    { vendor_vmware  , 0x0790 , device_class_pci_bridge              , "PCI bridge"                                   },
+    { vendor_vmware  , 0x07A0 , device_class_pci_bridge              , "PCI Express Root Port"                        },
+    { vendor_intel   , 0x100E , device_class_ethernet_controller     , "82540EM Gigabit Ethernet Controller"          },
+    { vendor_intel   , 0x100F , device_class_ethernet_controller     , "82545EM Gigabit Ethernet Controller (Copper)" },
+    { vendor_intel   , 0x1237 , device_class_host_bridge             , "440FX - 82441FX PMC [Natoma]"                 },
+    { vendor_intel   , 0x7000 , device_class_isa_bridge              , "82371SB PIIX3 ISA [Natoma/Triton II]"         },
+    { vendor_intel   , 0x7010 , device_class_ide_controller          , "82371SB PIIX3 IDE [Natoma/Triton II]"         },
+    { vendor_intel   , 0x7020 , device_class_usb_controller          , "82371SB PIIX3 USB [Natoma/Triton II]"         },
+    { vendor_intel   , 0x7110 , device_class_isa_bridge              , "82371AB/EB/MB PIIX4 ISA"                      },
+    { vendor_intel   , 0x7111 , device_class_ide_controller          , "82371AB/EB/MB PIIX4 IDE"                      },
+    { vendor_intel   , 0x7113 , device_class_other_bridge            , "82371AB/EB/MB PIIX4 ACPI"                     },
+    { vendor_intel   , 0x7190 , device_class_host_bridge             , "440BX/ZX/DX - 82443BX/ZX/DX Host bridge"      },
+    { vendor_intel   , 0x7191 , device_class_pci_bridge              , "440BX/ZX/DX - 82443BX/ZX/DX AGP bridge"       },
 };
 
 template<typename T, size_t size, typename Pred>
@@ -151,27 +178,57 @@ config_area read_config_area(device_address dev)
     return u.ca;
 }
 
+void probe_bus(uint8_t bus);
+
+void probe(device_address dev_addr)
+{
+    const auto cfgdw = read_config_dword(dev_addr, 0);
+    if ((cfgdw&0xffff) == 0xffff) {
+        return;
+    }
+    auto ca = read_config_area(dev_addr);
+    const auto header_type = ca.header_type & header_type_device_mask;
+    if (header_type == 0) {
+        // 0x00 = Normal devices
+        const char* vendor_text = "Unknown vendor";
+        const char* device_text = "Unknown device";
+        if (auto vi = get_vendor_info(ca.vendor)) {
+            vendor_text = vi->text;
+        }
+        if (auto di = get_device_info(ca)) {
+            device_text = di->text;
+        }
+        dbgout() << dev_addr << " " << as_hex(ca.device_class) << ": " << as_hex(ca.vendor) << ":" << as_hex(ca.device_id) << " " << vendor_text << " " << device_text << "\n";
+    } else if (header_type == 1) {
+        // 0x01 = PCI-to-PCI bridge
+        REQUIRE(dev_addr.bus() == ca.header1.primary_bus);
+        probe_bus(ca.header1.secondary_bus);
+    } else {
+        // 0x02 = CardBus bridge
+        dbgout() << dev_addr << " Unknown header type " << as_hex(ca.header_type) << "\n";
+        REQUIRE(false);
+    }
+
+    if (dev_addr.func() == 0 && (ca.header_type & header_type_multi_function_mask)) {
+        for (uint8_t func = 1; func < 8; ++func) {
+            probe(device_address{dev_addr.bus(), dev_addr.slot(), func});
+        }
+    }
+}
+
+void probe_bus(uint8_t bus)
+{
+    for (int slot = 0; slot < 32; ++slot) {
+        const auto dev_addr = device_address{static_cast<uint8_t>(bus), static_cast<uint8_t>(slot), 0};
+        probe(dev_addr);
+    }
+}
+
 class manager_impl : public manager {
 public:
     manager_impl() {
         dbgout() << "[pci] Initializing.\n";
-        for (int bus = 0; bus < 256; ++bus) {
-            for (int slot = 0; slot < 32; ++slot) {
-                const auto dev_adr = device_address{static_cast<uint8_t>(bus), static_cast<uint8_t>(slot), 0};
-                const auto cfgdw   = read_config_dword(dev_adr, 0);
-                if ((cfgdw&0xffff) == 0xffff) continue;
-                auto ca = read_config_area(dev_adr);
-                const char* vendor_text = "Unknown vendor";
-                const char* device_text = "Unknown device";
-                if (auto vi = get_vendor_info(ca.vendor)) {
-                    vendor_text = vi->text;
-                }
-                if (auto di = get_device_info(ca)) {
-                    device_text = di->text;
-                }
-                dbgout () << dev_adr << " " << as_hex(ca.device_class) << ": " << as_hex(ca.vendor) << ":" << as_hex(ca.device_id) << " " << vendor_text << " " << device_text << "\n";
-            }
-        }
+        probe_bus(0);
     }
     ~manager_impl() {
         dbgout() << "[pci] Shutting down.\n";
