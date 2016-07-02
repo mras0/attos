2207b7fc3f0d0d790888ed70852ca9ecc12c8aef
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2016-07-02 22:46:43 +0200

    Handle UWOP_ALLOC_LARGE

--- exp/read_pe/asm_test.asm
+++ exp/read_pe/asm_test.asm
@@ -11,11 +11,10 @@
 win64_proc test_fun
     win64_prologue_push RBP
     mov rbp, rsp
-    win64_prologue_alloc 0x20
+    win64_prologue_alloc 0x220
     win64_prologue_end
     call foo
-    ;add rsp, 0x20
-    ;pop rbp
     win64_epilogue
     ret
 win64_proc_end
+
